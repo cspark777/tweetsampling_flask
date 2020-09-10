51ab842d548cde6218e23856ceed041fb46bc080
Victor L <rhettb198501@hotmail.com>
2020-09-10 08:09:09 -0300

    fix

--- sampling/page/views.py
+++ sampling/page/views.py
@@ -25,7 +25,14 @@ mydb = mysql.connector.connect(
 
 countries = dict(countries_for_language('en'))
 
-def init_chart_data():
+def log_message(message):
+    #print(message)
+    
+    hs = open("web_log.txt","a", encoding="utf8")
+    hs.write(message + "\n")
+    hs.close() 
+
+def init_chart_data(keyword):
     global countries
 
     chart_data = {}
@@ -33,9 +40,12 @@ def init_chart_data():
     #--- for line chart
     line_chart_data = []
     current_time = datetime.datetime.utcnow()
-
-    for i in range(60):
-        delta = 59 - i
+    
+    current_time = datetime.datetime(2020, 9, 9, 4, 30, 00, 000000)
+    
+    query = ""
+    for i in range(20):
+        delta = 19 - i
         dt = current_time - datetime.timedelta(minutes=delta)
         calc_time = dt.strftime('%Y-%m-%d %H:%M:00')
 
@@ -43,102 +53,181 @@ def init_chart_data():
                 
         sec = int(time.mktime(ti)) * 1000
 
-        negative = random.randint(1,20)
-        neutral = random.randint(50,100)
-        positive = random.randint(1,20)
+        if keyword != "":
+            query = query + "(SELECT {} as sec, IFNULL(SUM(IF(polarity=-1, 1, 0)), 0) AS ng, IFNULL(SUM(IF(polarity=0, 1, 0)), 0) AS ne, IFNULL(SUM(IF(polarity=1, 1, 0)), 0) AS po FROM tweets WHERE keyword='{}' AND created_at < date_sub('{}',INTERVAL {} MINUTE) AND created_at>=date_sub('{}', INTERVAL {} MINUTE))".format(sec, keyword, current_time, delta, current_time, delta+1) + " UNION "
+        else:
+            query = query + "(SELECT {} as sec, IFNULL(SUM(IF(polarity=-1, 1, 0)), 0) AS ng, IFNULL(SUM(IF(polarity=0, 1, 0)), 0) AS ne, IFNULL(SUM(IF(polarity=1, 1, 0)), 0) AS po FROM tweets WHERE created_at < date_sub('{}',INTERVAL {} MINUTE) AND created_at>=date_sub('{}', INTERVAL {} MINUTE))".format(sec, current_time, delta, current_time, delta+1) + " UNION "
 
-        data = {"date":sec, "negative":negative, "neutral":neutral, "positive":-positive}
-        line_chart_data.append(data)
+    query = query[0:-7]
+    #print(query)
+    if mydb.is_connected():
+        mycursor = mydb.cursor()        
+        mycursor.execute(query)
+        graph_data = mycursor.fetchall()   
+        mycursor.close()  
+
+        for one in graph_data:
+            data = {"date":one[0], "negative":int(one[1]), "neutral":int(one[2]), "positive":-int(one[3])}
+            line_chart_data.append(data)
+    else:
+        log_message("--- {} : MySQL error : init line chart data error".format(current_time))
+        for i in range(20):
+            delta = 19 - i
+            dt = current_time - datetime.timedelta(minutes=delta)
+            calc_time = dt.strftime('%Y-%m-%d %H:%M:00')
+            ti = time.strptime(calc_time, "%Y-%m-%d %H:%M:00")                    
+            sec = int(time.mktime(ti)) * 1000
+            negative = random.randint(1,20)
+            neutral = random.randint(50,100)
+            positive = random.randint(1,20)
+            data = {"date":sec, "negative":negative, "neutral":neutral, "positive":-positive}
+            line_chart_data.append(data)
 
     chart_data["line_chart_data"] = line_chart_data
     
     #--- for pie chart
     dt = current_time
     calc_time = dt.strftime('%Y-%m-%d %H:%M:00')
-
     ti = time.strptime(calc_time, "%Y-%m-%d %H:%M:00")                
     sec = int(time.mktime(ti)) * 1000
-    negative = random.randint(1,20)
-    neutral = random.randint(50,100)
-    positive = random.randint(1,20)
-
-    data = {"date":sec, "negative":negative, "neutral":neutral, "positive":positive}
+    data = get_pie_chart(sec, keyword)
     chart_data["pie_chart_data"] = data
 
     #--- for bar chart
-    bar_chart_data = []
-    dt = current_time
-    calc_time = dt.strftime('%Y-%m-%d %H:%M:00')
-
-    country_list = ['us', 've', 'ph', 'mx', 'ca', 'id', 'in', 'au', 'br', 'co']
-    for i in range(10):
-        data = {"country": countries[country_list[i].upper()], "value":random.randint(1,100)}
-        bar_chart_data.append(data)
+    bar_chart_data = get_bar_chart(sec, keyword)
     chart_data["bar_chart_data"] = bar_chart_data
 
+    #--- for world chart
+    world_chart_data = get_world_chart(sec, keyword)
+    chart_data["world_chart_data"] = world_chart_data
 
     return chart_data
 
-def get_line_chart(next_time):    
+def get_line_chart(next_time, keyword):    
     next_time_obj = datetime.datetime.fromtimestamp(next_time/1000.0)
     next_time_str = next_time_obj.strftime("%Y-%m-%d %H:%M:00")
     
-    negative = random.randint(1,20)
-    neutral = random.randint(50,100)
-    positive = random.randint(1,20)
+    if keyword == "":
+        query = "(SELECT {} as sec, IFNULL(SUM(IF(polarity=-1, 1, 0)), 0) AS ng, IFNULL(SUM(IF(polarity=0, 1, 0)), 0) AS ne, IFNULL(SUM(IF(polarity=1, 1, 0)), 0) AS po FROM tweets WHERE created_at < date_sub('{}',INTERVAL {} MINUTE) AND created_at>=date_sub('{}', INTERVAL {} MINUTE))".format(next_time, next_time_str, 0, next_time_str, 1)
+    else:
+        query = "(SELECT {} as sec, IFNULL(SUM(IF(polarity=-1, 1, 0)), 0) AS ng, IFNULL(SUM(IF(polarity=0, 1, 0)), 0) AS ne, IFNULL(SUM(IF(polarity=1, 1, 0)), 0) AS po FROM tweets WHERE keyword='{}' AND created_at < date_sub('{}',INTERVAL {} MINUTE) AND created_at>=date_sub('{}', INTERVAL {} MINUTE))".format(next_time, keyword, next_time_str, 0, next_time_str, 1)
 
-    data = {"date":next_time, "negative":negative, "neutral":neutral, "positive":-positive}
+    if mydb.is_connected():
+        mycursor = mydb.cursor()        
+        mycursor.execute(query)
+        graph_data = mycursor.fetchone()   
+        mycursor.close() 
+
+        data = {"date":next_time, "negative":int(graph_data[1]), "neutral":int(graph_data[2]), "positive":-int(graph_data[3])}
+    else:
+        log_message("--- {} : MySQL error : init line chart data error".format(next_time_str))
+        negative = random.randint(1,20)
+        neutral = random.randint(50,100)
+        positive = random.randint(1,20)
+        data = {"date":next_time, "negative":negative, "neutral":neutral, "positive":-positive}
     return data
 
-def get_pie_chart(next_time):
-    next_time_obj = datetime.datetime.fromtimestamp(next_time/1000.0)
-    next_time_str = next_time_obj.strftime("%Y-%m-%d %H:%M:00")
-    
-    negative = random.randint(1,20)
-    neutral = random.randint(50,100)
-    positive = random.randint(1,20)
+def get_pie_chart(next_time, keyword):
+    return get_line_chart(next_time, keyword)
 
-    data = {"date":next_time, "negative":negative, "neutral":neutral, "positive":positive}
-    return data
-
-def get_bar_chart(next_time):
-    bar_chart_data = []
+def get_bar_chart(next_time, keyword):
     next_time_obj = datetime.datetime.fromtimestamp(next_time/1000.0)
     next_time_str = next_time_obj.strftime("%Y-%m-%d %H:%M:00")
 
-    country_list = ['us', 've', 'ph', 'mx', 'ca', 'id', 'in', 'au', 'br', 'co']
-    for i in range(10):
-        data = {"country": countries[country_list[i].upper()], "value":random.randint(1,100)}
-        bar_chart_data.append(data)
-    
+    bar_chart_data = []    
+    if keyword != "":
+        query = "SELECT country_code, COUNT(tweet_id) cc FROM tweets WHERE keyword='{}' AND created_at < '{}' AND created_at>=date_sub('{}', INTERVAL 1 MINUTE) GROUP BY country_code ORDER BY cc DESC LIMIT 0, 10".format(keyword, next_time_str, next_time_str)
+    else:
+        query = "SELECT country_code, COUNT(tweet_id) cc FROM tweets WHERE created_at < '{}' AND created_at>=date_sub('{}', INTERVAL 1 MINUTE) GROUP BY country_code ORDER BY cc DESC LIMIT 0, 10".format(keyword, next_time_str, next_time_str)
+
+    if mydb.is_connected():
+        mycursor = mydb.cursor()        
+        mycursor.execute(query)
+        graph_data = mycursor.fetchall()   
+        mycursor.close() 
+        for one in graph_data:
+            data = {"country": one[0], "value":one[1]} 
+            bar_chart_data.append(data)
+    else:
+        log_message("--- {} : MySQL error : Get bar chart data error".format(current_time))
+        country_list = ['us', 've', 'ph', 'mx', 'ca', 'id', 'in', 'au', 'br', 'co']
+        for i in range(10):
+            data = {"country": countries[country_list[i].upper()], "value":random.randint(1,100)}
+            bar_chart_data.append(data)
+        
     return bar_chart_data
 
+def get_world_chart(next_time, keyword):
+    next_time_obj = datetime.datetime.fromtimestamp(next_time/1000.0)
+    next_time_str = next_time_obj.strftime("%Y-%m-%d %H:%M:00")
 
-def get_chart(next_time):
+    world_chart_data = []    
+    if keyword != "":
+        query = "SELECT UPPER(country_code), COUNT(tweet_id) cc FROM tweets WHERE keyword='{}' AND created_at<'{}' GROUP BY country_code ORDER BY cc".format(keyword, next_time_str)
+    else:
+        query = "SELECT UPPER(country_code), COUNT(tweet_id) cc FROM tweets AND created_at<'{}' GROUP BY country_code ORDER BY cc".format(next_time_str)
+
+    print(query)
+
+    if mydb.is_connected():
+        mycursor = mydb.cursor()        
+        mycursor.execute(query)
+        graph_data = mycursor.fetchall()   
+        mycursor.close() 
+        for one in graph_data:
+            data = {"id": one[0], "value":one[1]} 
+            world_chart_data.append(data)
+    else:
+        log_message("--- {} : MySQL error : Get world chart data error".format(current_time))
+        country_list = ['US', 'VE', 'PH', 'MX', 'CA', 'ID', 'IN', 'AU', 'BR', 'CO']
+        for i in range(10):
+            data = {"id": countries[country_list[i].upper()], "value":random.randint(1,100)}
+            world_chart_data.append(data)
+        
+    return world_chart_data
+
+
+def get_chart(next_time, keyword):
     chart_data = {}
 
-    line_chart_data = get_line_chart(next_time)
+    line_chart_data = get_line_chart(next_time, keyword)
     chart_data["line_chart_data"] = line_chart_data
 
-    pie_chart_data = get_pie_chart(next_time)
+    pie_chart_data = get_pie_chart(next_time, keyword)
     chart_data["pie_chart_data"] = pie_chart_data
 
-    bar_chart_data = get_bar_chart(next_time)
+    bar_chart_data = get_bar_chart(next_time, keyword)
     chart_data["bar_chart_data"] = bar_chart_data
+
+    world_chart_data = get_world_chart(next_time, keyword)
+    chart_data["world_chart_data"] = world_chart_data
 
     return chart_data
     
     
 @page.route('/', methods=['GET', 'POST'])
 def index():  
+    f = open("tweet_key.txt", "r")
+    search_words = f.read()
 
-    chart_data = init_chart_data()
+    chart_data = init_chart_data(search_words)
     chart_data = json.dumps(chart_data) 
 
-    return render_template('index.html', chart_data=chart_data)
+    return render_template('index.html', chart_data=chart_data, keyword=search_words)
 
 @page.route('/chart_data', methods=['POST'])
 def chart_data():
+    f = open("tweet_key.txt", "r")
+    search_words = f.read()
+
     next_time = request.form.get("next_time")
-    json_obj = get_chart(int(next_time))
+    json_obj = get_chart(int(next_time), search_words)
     return jsonify(json_obj)
+
+@page.route('/keyword', methods=['POST'])
+def keyword():
+    keyword = request.form.get("keyword")
+    if keyword != "":
+        f = open("tweet_key.txt", "w")
+        f.write(keyword)
+    return redirect("/", code=302)
--- sampling/static/js/custom.js
+++ sampling/static/js/custom.js
@@ -6,7 +6,7 @@
 
     am4core.useTheme(am4themes_animated);
     //am4core.useTheme(am4themes_dark);
-
+am4core.ready(function() {
     var data = [];
     
     var data_json = $("#init_chart_data").val();
@@ -171,6 +171,8 @@
 
         var valueAxis_bar = chart_bar.xAxes.push(new am4charts.ValueAxis());
         valueAxis_bar.min = 0;        
+
+        
         //valueAxis_bar.rangeChangeEasing = am4core.ease.linear;
         //valueAxis_bar.rangeChangeDuration = 1500;
 
@@ -199,84 +201,308 @@
         categoryAxis_bar.sortBySeries = series_bar;
 
         //===== for world map chart
-        var chart = am4core.create("chartdiv_map", am4maps.MapChart);
+        var circleColor = am4core.color("#45d21a");
+        var countryColor = am4core.color("#3b3b3b");
+        var countryStrokeColor = am4core.color("#000000");
+        var countryHoverColor = am4core.color("#1b1b1b");
+        var activeCountryColor = am4core.color("#0f0f0f");
+        var currentCountry = "World";
+        var currentPolygon;
 
+        var world_chart_data = data_obj["world_chart_data"]
 
-        try {
-            chart.geodata = am4geodata_worldLow;
+        // as we will be modifying raw data, make a copy
+        var mapData = JSON.parse(JSON.stringify(world_chart_data));
+
+        // remove items with 0 values for better performance
+        for(var i = mapData.length - 1; i >= 0; i--){
+            if(mapData[i].value == 0){
+                mapData.splice(i, 1);
+            }
         }
-        catch (e) {
-            chart.raiseCriticalError(new Error("Map geodata could not be loaded. Please download the latest <a href=\"https://www.amcharts.com/download/download-v4/\">amcharts geodata</a> and extract its contents into the same directory as your amCharts files."));
-        }
+
+        // main container
+        // https://www.amcharts.com/docs/v4/concepts/svg-engine/containers/
+        var container = am4core.create("chartdiv_map", am4core.Container);
+        container.width = am4core.percent(100);
+        container.height = am4core.percent(100);
+
+        container.tooltip = new am4core.Tooltip();  
+        container.tooltip.background.fill = am4core.color("#000000");
+        container.tooltip.background.stroke = circleColor;
+        container.tooltip.fontSize = "0.9em";
+        container.tooltip.getFillFromObject = false;
+        container.tooltip.getStrokeFromObject = false;
+
+        // MAP CHART 
+        // https://www.amcharts.com/docs/v4/chart-types/map/
+        var mapChart = container.createChild(am4maps.MapChart);
+        mapChart.height = am4core.percent(100);
+        mapChart.zoomControl = new am4maps.ZoomControl();
+        mapChart.zoomControl.align = "right";
+        mapChart.zoomControl.marginRight = 15;
+        mapChart.zoomControl.valign = "middle";
+        mapChart.homeGeoPoint = { longitude: 0, latitude: -2 };
+
+        // by default minus button zooms out by one step, but we modify the behavior so when user clicks on minus, the map would fully zoom-out and show world data
+        mapChart.zoomControl.minusButton.events.on("hit", showWorld);
+        // clicking on a "sea" will also result a full zoom-out
+        mapChart.seriesContainer.background.events.on("hit", showWorld);
+        mapChart.seriesContainer.background.events.on("over", resetHover);
+        mapChart.seriesContainer.background.fillOpacity = 0;
+        mapChart.zoomEasing = am4core.ease.sinOut;
+
+        // https://www.amcharts.com/docs/v4/chart-types/map/#Map_data
+        // you can use more accurate world map or map of any other country - a wide selection of maps available at: https://github.com/amcharts/amcharts4-geodata
+        mapChart.geodata = am4geodata_worldLow;
+
+        // Set projection
+        // https://www.amcharts.com/docs/v4/chart-types/map/#Setting_projection
+        // instead of Miller, you can use Mercator or many other projections available: https://www.amcharts.com/demos/map-using-d3-projections/
+        mapChart.projection = new am4maps.projections.Miller();
+        mapChart.panBehavior = "move";
+
+        // when map is globe, beackground is made visible
+        mapChart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 0.05;
+        mapChart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#ffffff");
+        mapChart.backgroundSeries.hidden = true;
 
 
-        chart.projection = new am4maps.projections.Miller();
+        // Map polygon series (defines how country areas look and behave)
+        var polygonSeries = mapChart.series.push(new am4maps.MapPolygonSeries());
+        polygonSeries.dataFields.id = "id";
+        polygonSeries.dataFields.value = "valuePC";
+        polygonSeries.interpolationDuration = 0;
 
-        var title = chart.chartContainer.createChild(am4core.Label);
-        title.text = "Life expectancy in the World";
-        title.fontSize = 20;
-        title.paddingTop = 30;
-        title.align = "center";
-
-        var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-        var polygonTemplate = polygonSeries.mapPolygons.template;
-        polygonTemplate.tooltipText = "{name}: {value.value}";
+        polygonSeries.exclude = ["AQ"]; // Antarctica is excluded in non-globe projection
         polygonSeries.useGeodata = true;
-        polygonSeries.heatRules.push({ property: "fill", target: polygonSeries.mapPolygons.template, min: am4core.color("#ffffff"), max: am4core.color("#AAAA00") });
+        polygonSeries.nonScalingStroke = true;
+        polygonSeries.strokeWidth = 0.5;
+        // this helps to place bubbles in the visual middle of the area
+        polygonSeries.calculateVisualCenter = true;
+        polygonSeries.data = mapData;
+
+        var polygonTemplate = polygonSeries.mapPolygons.template;
+        polygonTemplate.fill = countryColor;
+        polygonTemplate.fillOpacity = 1
+        polygonTemplate.stroke = countryStrokeColor;
+        polygonTemplate.strokeOpacity = 0.15
+        polygonTemplate.setStateOnChildren = true;
+        polygonTemplate.tooltipPosition = "fixed";
+
+        polygonTemplate.events.on("over", handleCountryOver);
+        polygonTemplate.events.on("out", handleCountryOut);
 
 
-        // add heat legend
-        var heatLegend = chart.chartContainer.createChild(am4maps.HeatLegend);
-        heatLegend.valign = "bottom";
-        heatLegend.series = polygonSeries;
-        heatLegend.width = am4core.percent(100);
-        heatLegend.orientation = "horizontal";
-        heatLegend.padding(30, 30, 30, 30);
-        heatLegend.valueAxis.renderer.labels.template.fontSize = 10;
-        heatLegend.valueAxis.renderer.minGridDistance = 40;
-
-        polygonSeries.mapPolygons.template.events.on("over", function (event) {
-          handleHover(event.target);
+        polygonSeries.heatRules.push({
+            "target": polygonTemplate,
+            "property": "fill",
+            "min": countryColor,
+            "max": countryColor,
+            "dataField": "value"
         })
 
-        polygonSeries.mapPolygons.template.events.on("hit", function (event) {
-          handleHover(event.target);
+        // you can have pacific - centered map if you set this to -154.8
+        mapChart.deltaLongitude = -10;
+
+        // polygon states
+        var polygonHoverState = polygonTemplate.states.create("hover");
+        polygonHoverState.transitionDuration = 1400;
+        polygonHoverState.properties.fill = countryHoverColor;
+
+        var polygonActiveState = polygonTemplate.states.create("active")
+        polygonActiveState.properties.fill = activeCountryColor;
+
+        // Bubble series
+        var bubbleSeries = mapChart.series.push(new am4maps.MapImageSeries());  
+        bubbleSeries.data = JSON.parse(JSON.stringify(mapData));
+
+        bubbleSeries.dataFields.value = "value";
+        bubbleSeries.dataFields.id = "id";
+
+        // adjust tooltip
+        bubbleSeries.tooltip.animationDuration = 0;
+        bubbleSeries.tooltip.showInViewport = false;
+        bubbleSeries.tooltip.background.fillOpacity = 0.2;
+        bubbleSeries.tooltip.getStrokeFromObject = true;
+        bubbleSeries.tooltip.getFillFromObject = false;
+        bubbleSeries.tooltip.background.fillOpacity = 0.2;
+        bubbleSeries.tooltip.background.fill = am4core.color("#000000");
+
+        var imageTemplate = bubbleSeries.mapImages.template;
+        // if you want bubbles to become bigger when zoomed, set this to false
+        imageTemplate.nonScaling = true;
+        imageTemplate.strokeOpacity = 0;
+        imageTemplate.fillOpacity = 0.55;
+        imageTemplate.tooltipText = "{name}: [bold]{value}[/]";
+        imageTemplate.applyOnClones = true;
+
+        imageTemplate.events.on("over", handleImageOver);
+        imageTemplate.events.on("out", handleImageOut);
+
+        // this is needed for the tooltip to point to the top of the circle instead of the middle
+        imageTemplate.adapter.add("tooltipY", function(tooltipY, target) {
+            return -target.children.getIndex(0).radius;
         })
 
-        function handleHover(mapPolygon) {
-          if (!isNaN(mapPolygon.dataItem.value)) {
-            heatLegend.valueAxis.showTooltipAt(mapPolygon.dataItem.value)
-          }
-          else {
-            heatLegend.valueAxis.hideTooltip();
-          }
+        // When hovered, circles become non-opaque  
+        var imageHoverState = imageTemplate.states.create("hover");
+        imageHoverState.properties.fillOpacity = 1;
+
+        // add circle inside the image
+        var circle = imageTemplate.createChild(am4core.Circle);
+        // this makes the circle to pulsate a bit when showing it
+        circle.hiddenState.properties.scale = 0.0001;
+        circle.hiddenState.transitionDuration = 2000;
+        circle.defaultState.transitionDuration = 2000;
+        circle.defaultState.transitionEasing = am4core.ease.elasticOut;
+        // later we set fill color on template (when changing what type of data the map should show) and all the clones get the color because of this
+        circle.applyOnClones = true;
+
+        // heat rule makes the bubbles to be of a different width. Adjust min/max for smaller/bigger radius of a bubble
+        bubbleSeries.heatRules.push({
+            "target": circle,
+            "property": "radius",
+            "min": 3,
+            "max": 15,
+            "dataField": "value"
+        })
+
+        // when data items validated, hide 0 value bubbles (because min size is set)
+        bubbleSeries.events.on("dataitemsvalidated", function() {
+            bubbleSeries.dataItems.each((dataItem) => {
+                var mapImage = dataItem.mapImage;
+                var circle = mapImage.children.getIndex(0);
+                if (mapImage.dataItem.value == 0) {
+                    circle.hide(0);
+                }
+                else if (circle.isHidden || circle.isHiding) {
+                    circle.show();
+                }
+            })
+        })
+
+        // setting colors on mapImage for tooltip colors
+        bubbleSeries.mapImages.template.fill = circleColor
+        bubbleSeries.mapImages.template.stroke = circleColor
+        // first child is circle
+        bubbleSeries.mapImages.template.children.getIndex(0).fill = circleColor
+
+        // this places bubbles at the visual center of a country
+        imageTemplate.adapter.add("latitude", function(latitude, target) {
+        var polygon = polygonSeries.getPolygonById(target.dataItem.id);
+            if (polygon) {
+                target.disabled = false;
+                return polygon.visualLatitude;
+            }
+            else {
+                target.disabled = true;
+            }
+            return latitude;
+        })
+
+        imageTemplate.adapter.add("longitude", function(longitude, target) {
+        var polygon = polygonSeries.getPolygonById(target.dataItem.id);
+            if (polygon) {
+                target.disabled = false;
+                return polygon.visualLongitude;
+            }
+            else {
+                target.disabled = true;
+            }
+            return longitude;
+        })
+
+        // END OF MAP  
+
+        // what happens when a country is rolled-over
+        function rollOverCountry(mapPolygon) {
+
+            resetHover();
+            if (mapPolygon) {
+                mapPolygon.isHover = true;
+
+                // make bubble hovered too
+                var image = bubbleSeries.getImageById(mapPolygon.dataItem.id);
+                if (image) {
+                    image.dataItem.dataContext.name = mapPolygon.dataItem.dataContext.name;
+                    image.isHover = true;
+                }
+            }
+        }
+        // what happens when a country is rolled-out
+        function rollOutCountry(mapPolygon) {
+            var image = bubbleSeries.getImageById(mapPolygon.dataItem.id)
+
+            resetHover();
+                if (image) {
+                    image.isHover = false;
+                }
         }
 
-        polygonSeries.mapPolygons.template.events.on("out", function (event) {
-          heatLegend.valueAxis.hideTooltip();
-        })
+        // show world data
+        function showWorld() {
+            currentCountry = "World";
+            currentPolygon = undefined;
+            resetHover();
 
+            // make all inactive
+            polygonSeries.mapPolygons.each(function(polygon) {
+                polygon.isActive = false;
+            })
 
-        // life expectancy data
+            mapChart.goHome();
+        }
 
-        polygonSeries.data = [{ id: "AF", value: 60.524 },
-        { id: "AL", value: 77.185 },
-        { id: "DZ", value: 70.874 },
-        { id: "AO", value: 51.498 },
-        { id: "AR", value: 76.128 },
-        { id: "AM", value: 74.469 },
-        { id: "AU", value: 82.364 },
-        { id: "AT", value: 80.965 },
-        { id: "AZ", value: 70.686 },
-        { id: "BH", value: 76.474 },
-        { id: "BD", value: 70.258 },
-        { id: "BY", value: 69.829 },
-        { id: "BE", value: 80.373 },
-        { id: "BJ", value: 59.165 },
-        ];
+        function handleImageOver(event) {
+            rollOverCountry(polygonSeries.getPolygonById(event.target.dataItem.id));
+        }
 
-        // excludes Antarctica
-        polygonSeries.exclude = ["AQ"];
+        function handleImageOut(event) {
+            rollOutCountry(polygonSeries.getPolygonById(event.target.dataItem.id));
+        }
+
+        function handleCountryOver(event) {
+            rollOverCountry(event.target);
+        }
+
+        function handleCountryOut(event) {
+            rollOutCountry(event.target);
+        }
+
+        function resetHover() {
+            polygonSeries.mapPolygons.each(function(polygon) {
+                polygon.isHover = false;
+            })
+
+            bubbleSeries.mapImages.each(function(image) {
+                image.isHover = false;
+            })
+        }
+
+        function updateMapData(data) {
+            //modifying instead of setting new data for a nice animation
+            bubbleSeries.dataItems.each(function(dataItem) {
+                dataItem.dataContext.value = 0;        
+            })            
+
+            for (var i = 0; i < data.length; i++) {
+                var di = data[i];
+                var image = bubbleSeries.getImageById(di.id);
+                var polygon = polygonSeries.getPolygonById(di.id);
+
+                if (image) {                    
+                    image.dataItem.dataContext.value = di.value;
+                }
+
+                if (polygon) {                
+                    polygon.dataItem.dataContext.valuePC = di.value;                
+                }
+
+                bubbleSeries.invalidateRawData();
+                polygonSeries.invalidateRawData();
+            }
+        }
 
         //===========================
 
@@ -329,12 +555,12 @@
                     label_pie.text = label_string;
                     chart_pie.invalidateRawData();
 
-
                     var bar_chart_data = msg["bar_chart_data"];
                     chart_bar.data = bar_chart_data;
                     chart_pie.invalidateRawData();
 
-
+                    var world_chart_data = msg["world_chart_data"];
+                    updateMapData(world_chart_data);
                 })
                 .fail(function(msg){
                     console.log(msg);          
@@ -345,5 +571,7 @@
 
         startInterval();
     }
+
+});
  
 })(jQuery); // End of use strict
